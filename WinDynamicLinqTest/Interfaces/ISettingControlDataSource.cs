b2524c0aad2a21607574dba150a74c350aec3fa5
kazenetu <rakudafactory@gmail.com>
2017-10-15 15:49:41 +0900

    入力コントロール用データソース取得インターフェースを追加

---
+++ WinDynamicLinqTest/Interfaces/ISettingControlDataSource.cs
@@ -0,0 +1,28 @@
+﻿namespace WinDynamicLinqTest.Interfaces
+{
+    /// <summary>
+    /// 入力コントロール用データソース取得インターフェース
+    /// </summary>
+    public interface ISettingControlDataSource
+
+    {
+        /// <summary>
+        /// 選択値を取得
+        /// </summary>
+        /// <returns></returns>
+        dynamic GetItem();
+
+        /// <summary>
+        /// 表示プロパティ名
+        /// </summary>
+        /// <returns></returns>
+        string GetDisplayMember();
+
+        /// <summary>
+        /// 設定値プロパティ名
+        /// </summary>
+        /// <returns></returns>
+        string GetValueMember();
+
+    }
+}
