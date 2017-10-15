c36b87efff7afe5178fdb70f744f9dae673f6565
kazenetu <rakudafactory@gmail.com>
2017-10-15 15:47:19 +0900

    入力用属性を追加

---
+++ WinDynamicLinqTest/Attributes/DisplayOrderAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WinDynamicLinqTest.Attributes
+{
+    /// <summary>
+    /// コントロール表示用属性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class DisplayOrderAttribute : Attribute
+    {
+        /// <summary>
+        /// 表示順番
+        /// </summary>
+
+        public int Order { get; set; }
+    }
+}
---
+++ WinDynamicLinqTest/Attributes/InputControlAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinDynamicLinqTest.Attributes
+{
+    /// <summary>
+    /// 入力コントロール定義用属性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class InputControlAttribute : Attribute
+    {
+        /// <summary>
+        /// ラベル表示名
+        /// </summary>
+        public string LabelText { get; set; }
+
+        /// <summary>
+        /// 入力コントロール：種類
+        /// </summary>
+        public Type InputControl { get; set; }
+
+        /// <summary>
+        /// 入力コントロール：選択リストを定義するクラス
+        /// </summary>
+        /// <remarks>コンボボックスやリストボックスの選択情報</remarks>
+        public Type DataSourceClass { get; set; }
+    }
+}
