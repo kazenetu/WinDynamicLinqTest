cbe51b2746c22dc8f68ea88518629b5c727452c6
kazenetu <rakudafactory@gmail.com>
2017-10-15 15:27:06 +0900

    テスト用ViewModelを追加

---
+++ WinDynamicLinqTest/ViewModels/TestVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace WinDynamicLinqTest.ViewModels
+{
+    /// <summary>
+    /// コントロール表示、バインドテスト用クラス
+    /// </summary>
+    public class TestVM
+    {
+        [Description("名前")]
+        public string Name { set; get; }
+
+        [Description("値")]
+        public string Value { set; get; }
+    }
+}
