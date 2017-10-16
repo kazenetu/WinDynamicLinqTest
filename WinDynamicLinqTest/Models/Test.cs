7fe6ad58da27ba9aae3984f7bc5fa11546cf906e
kazenetu <rakudafactory@gmail.com>
2017-10-16 21:46:47 +0900

    Modelクラス追加

---
+++ WinDynamicLinqTest/Models/Test.cs
@@ -0,0 +1,9 @@
+﻿namespace WinDynamicLinqTest.Models
+{
+    public class Test
+    {
+        public string Name { set; get; }
+
+        public string Value { set; get; }
+    }
+}
---
+++ WinDynamicLinqTest/Models/TestEx.cs
@@ -0,0 +1,11 @@
+﻿namespace WinDynamicLinqTest.Models
+{
+    public class TestEx
+    {
+        public string Name { set; get; }
+
+        public bool Value { set; get; }
+
+        public int SelectItem { set; get; }
+    }
+}
