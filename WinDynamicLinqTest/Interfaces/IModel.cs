906ab08b8af7f8c6558eab8656c7bc1bee1d1434
kazenetu <rakudafactory@gmail.com>
2017-10-17 22:16:55 +0900

    ModelとViewModel用インターフェース追加

---
+++ WinDynamicLinqTest/Interfaces/IModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WinDynamicLinqTest.Interfaces
+{
+    /// <summary>
+    /// モデルインスタンス
+    /// </summary>
+    public interface IModel
+    {
+        /// <summary>
+        /// ModelのTypeを返す
+        /// </summary>
+        /// <returns>ModelのType</returns>
+        Type GetModelType();
+    }
+}
---
+++ WinDynamicLinqTest/Interfaces/IViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WinDynamicLinqTest.Interfaces
+{
+    /// <summary>
+    /// ViewModelインターフェース
+    /// </summary>
+    public interface IViewModel
+    {
+        /// <summary>
+        /// Modelインスタンスの取得
+        /// </summary>
+        /// <returns>IModelインスタンス</returns>
+        IModel GetModel();
+    }
+}
