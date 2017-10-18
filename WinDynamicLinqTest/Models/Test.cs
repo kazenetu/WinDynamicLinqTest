9be80089eafdbf4599e65fdab5c33eb09c7e323c
kazenetu <rakudafactory@gmail.com>
2017-10-18 22:07:14 +0900

    ModelクラスにIModelインターフェースを継承するように修正

--- WinDynamicLinqTest/Models/Test.cs
+++ WinDynamicLinqTest/Models/Test.cs
@@ -1,9 +1,26 @@
-﻿namespace WinDynamicLinqTest.Models
+﻿using System;
+
+namespace WinDynamicLinqTest.Models
 {
-    public class Test
+    public class Test : Interfaces.IModel
     {
+        /// <summary>
+        /// ModelのTypeを返す
+        /// </summary>
+        /// <returns>ModelのType</returns>
+        public Type GetModelType()
+        {
+            return GetType();
+        }
+
+        /// <summary>
+        /// 文字列サンプル1
+        /// </summary>
         public string Name { set; get; }
 
+        /// <summary>
+        /// 文字列サンプル1
+        /// </summary>
         public string Value { set; get; }
     }
 }
--- WinDynamicLinqTest/Models/TestEx.cs
+++ WinDynamicLinqTest/Models/TestEx.cs
@@ -1,11 +1,31 @@
-﻿namespace WinDynamicLinqTest.Models
+﻿using System;
+
+namespace WinDynamicLinqTest.Models
 {
-    public class TestEx
+    public class TestEx : Interfaces.IModel
     {
+        /// <summary>
+        /// ModelのTypeを返す
+        /// </summary>
+        /// <returns>ModelのType</returns>
+        public Type GetModelType()
+        {
+            return GetType();
+        }
+
+        /// <summary>
+        /// 文字列サンプル
+        /// </summary>
         public string Name { set; get; }
 
+        /// <summary>
+        /// boolサンプル
+        /// </summary>
         public bool Value { set; get; }
 
+        /// <summary>
+        /// 数値サンプル
+        /// </summary>
         public int SelectItem { set; get; }
     }
 }
