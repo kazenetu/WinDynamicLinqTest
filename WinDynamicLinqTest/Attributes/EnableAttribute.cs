1fe8842121c65cb99ef1e52a33c7670fc8294ad8
kazenetu <rakudafactory@gmail.com>
2017-10-27 23:05:53 +0900

    コントロールの有効制御用属性を追加

---
+++ WinDynamicLinqTest/Attributes/EnableAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WinDynamicLinqTest.Attributes
+{
+    /// <summary>
+    /// 対象プロパティに紐づくコントロールの有効/無効制御
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class EnableAttribute:Attribute 
+    {
+        /// <summary>
+        /// 対象のプロパティ名
+        /// </summary>
+        public string targetPropertyName { set; get; }
+    }
+}
